fb568c613be3671ba1d2ca3d4d64e7e98bc4f2dc
TaZz06 <40108@ufp.edu.pt>
2020-12-12 18:25:03 +0000

    Project Creation

---
+++ cmake-build-debug/CMakeFiles/Projeto_LP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto_LP.dir/main.c.obj"
+  "Projeto_LP.exe"
+  "Projeto_LP.exe.manifest"
+  "Projeto_LP.pdb"
+  "libProjeto_LP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto_LP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Projeto_LP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Projeto_LP.
+# This may be replaced when dependencies are built.
